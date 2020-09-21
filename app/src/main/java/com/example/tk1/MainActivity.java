3ce64a496d0669281bc0a26fa480fce9fca7be59
NguyenVanHoang19 <nguyenvanhoang34iuh@gmail.com>
2020-09-22 01:00:49 +0700

    update mainactivity

--- app/src/main/java/com/example/tk1/MainActivity.java
+++ app/src/main/java/com/example/tk1/MainActivity.java
@@ -9,7 +9,7 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
-
+//update
 public class MainActivity extends AppCompatActivity {
     private EditText edtSoA,edtSoB,edtSoC;
     private TextView txvKetQua;
